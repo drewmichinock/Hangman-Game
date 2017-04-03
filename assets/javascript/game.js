76b7378f94684a65798c64ea52b08dd0ee1d6376
drewmichinock <drewmichinock@gmail.com>
2017-04-02 18:27:12 -0700

    Add existing file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,53 +11,21 @@ var city = ["austin", "boston", "chicago", "dallas", "denver",
     "detroit", "fresno", "houston", "memphis", "mesa"
 ];
 
-var guess;			// user guess
-var guesses = [];   // stored guesses
-var chances; 		// chances to guess correct letter
-var counter; 		// correct guesses
+var guess; // user guess
+var guesses = []; // stored guesses
+var chances; // chances to guess correct letter
+var counter; // correct guesses
 
 // FUNCTIONS //
 
 // select random city from city array
-/*function cityRandom () {
-	var citySelected = city[Math.floor(Math.random() * 10)];
-	return citySelected;
-}*/
-
-/*
-function multiplier(x, y, z) {
-
-  // The function should multiply the arguments, and log the result to the console.
-  console.log(x * y * z);
-}
-
-multiplier(2, 3, 4);
-/*
-
-/*
-function cityRandom() {
-
-return city[Math.floor(Math.random() * city.length)];
-
-};
-
-console.log()
-*/
-
 
 function cityRandom() {
-
-   return city[Math.floor(Math.random() * city.length)];
-
+    return city[Math.floor(Math.random() * city.length)];
 }
-
-   console.log(cityRandom());
-
-
 
 // blanks for user answers
 
 // replacing letters
 
 // guessing letters
-
