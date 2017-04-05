daff9b559f8eba73e3a96701b8afb35db469baed
Drew Michinock <drewmichinock@gmail.com>
2017-04-05 02:01:43 -0700

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,156 @@
-// VARIABLES //
-
-// letters user can choose from
-var letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-    "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
-    "x", "c", "v", "b", "n", "m",
+// cities for guessing
+var cities = ["albuquerque", "anaheim", "anchorage", "arlington", "atlanta",
+    "aurora", "austin", "bakersfield", "baltimore", "boston",
+    "charlotte", "chicago", "cincinnati", "cleveland", "columbus",
+    "dallas", "denver", "detroit", "fresno", "greensboro", "henderson",
+    "honolulu", "houston", "indianapolis", "jacksonville",
+    "lexington", "louisville", "memphis", "mesa", "miami", "milwaukee",
+    "minneapolis", "nashville", "oakland", "omaha", "philadelphia",
+    "phoenix", "pittsburgh", "plano", "portland", "raleigh", "riverside",
+    "sacramento", "seattle", "stockton", "tampa", "tucson", "tulsa",
+    "washington", "wichita"
 ];
+
+// stores guesses
+var guesses = [];
+
+// chances to guess correct letter	
+var chances = 12;
 
 // city selected for guessing
-var city = ["austin", "boston", "chicago", "dallas", "denver",
-    "detroit", "fresno", "houston", "memphis", "mesa"
-];
+var randomCity;
 
-var guess; // user guess
-var guesses = []; // stored guesses
-var chances; // chances to guess correct letter
-var counter; // correct guesses
+// guess progress (keeps underscores)
+var guess = "";
 
-// FUNCTIONS //
+var wins = "0";
 
-// select random city from city array
-
+// select random from cities array
 function cityRandom() {
-    return city[Math.floor(Math.random() * city.length)];
+    return cities[Math.floor(Math.random() * cities.length)];
 }
 
-// blanks for user answers
+// function called when page loaded (onReady)
+// when chances <= 0 in displayChances(), reset game values, and display new values.
+function startGame() {
 
-// replacing letters
+    randomCity = cityRandom();
 
-// guessing letters
+    chances = 12;
+
+    guesses = [];
+
+    guess = "";
+
+    wins = "";
+
+    for (var i = 0; i < randomCity.length; i++) {
+
+        guess += "_";
+    }
+
+    displayChances();
+
+    displayGuesses();
+
+    displayGuess();
+}
+
+// displays chances if > 0; otherwise alert "Game over" reset game
+function displayChances() {
+
+    document.getElementById("chances").innerHTML = "Guesses Remaining: " + chances;
+
+    // wait 500 milliseconds before checking chances; allows screen to update before alert displays
+    setTimeout(function() {
+
+        if (chances <= 0) {
+
+            alert("Game over. Try again.");
+
+            startGame();
+        }
+
+    }, 500);
+
+}
+
+//displays incorrect guesses
+function displayGuesses() {
+
+    document.getElementById("lettersGuessed").innerHTML = guesses.toString();
+
+}
+
+// displays current progress of user's game
+function displayGuess() {
+
+    var guessWithSpaces = "";
+
+    //b__t__
+    for (var i = 0; i < guess.length; i++) {
+
+        guessWithSpaces += guess.charAt(i) + " ";
+
+    }
+
+    document.getElementById("hangman").innerHTML = guessWithSpaces;
+
+    setTimeout(function() {
+
+        if (guess.indexOf("_") === -1) {
+
+            //alert("You win! Congratulations.");
+
+
+
+            startGame();
+        }
+
+    }, 500);
+
+}
+
+// fires on onkeyup and checks validity of guess
+function acceptGuess(event) {
+
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+
+    var index = randomCity.indexOf(letter);
+
+    if (index === -1) {
+
+        guesses[guesses.length] = letter;
+
+        chances--;
+
+        displayGuesses();
+
+        displayChances();
+
+    } else {
+
+        while (index !== -1) {
+            guess = replaceAt(guess, index, letter);
+
+            randomCity = replaceAt(randomCity, index, "_");
+
+            index = randomCity.indexOf(letter);
+        }
+        displayGuess();
+    }
+}
+
+// convenience function to replace a letter in a string
+function replaceAt(string, index, letter) {
+
+    //return ___                       t        __
+    return string.substr(0, index) + letter + string.substr(index + letter.length);
+
+}
+
+$(document).ready(function() {
+
+    startGame();
+
+});
